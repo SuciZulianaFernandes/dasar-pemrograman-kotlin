c632910f7b58452490a1eca4ce93e2acc97a82f1
suciifrndzz <sucizulianafernandes298@gmail.com>
2024-09-05 18:02:48 +0700

    Add BlokdanKomentar1.kt

---
+++ src/main/kotlin/BlokdanKomentar1.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("Hello, ini program pertama saya di kotlin")
+    //contoh komentar 1 baris
+    /*
+    contoh komentar multi baris
+     */
+    /**
+     * kotlin doc --> baris ini untuk mendokumentasikan program
+     */
+}
